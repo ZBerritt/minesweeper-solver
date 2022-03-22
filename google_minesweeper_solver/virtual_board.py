e73bd11afd867953f15cd5577811f679ab70d061
Zberritt <zberritt@gmail.com>
2022-03-22 17:42:19 -0400

    Fixed my earlier mistakes

--- google_minesweeper_solver/virtual_board.py
+++ google_minesweeper_solver/virtual_board.py
@@ -20,46 +20,51 @@ class Board:
 
     def get_space(self, x, y):
         return self.board[y][x]
-    
+
     def get_surrounding_tiles(self, x, y):
         tiles = []
         if x != 0 and y != 0:
-            tiles.append((x - 1, y - 1)) # Above-Left
+            tiles.append((x - 1, y - 1, self.get_space(x, y)))  # Above-Left
         if y != 0:
-            tiles.append((x, y - 1)) # Above
+            tiles.append((x, y - 1, self.get_space(x, y)))  # Above
         if x != self.horizontal_tiles - 1 and y != 0:
-            tiles.append((x + 1, y - 1)) # Above-Right
+            tiles.append((x + 1, y - 1, self.get_space(x, y)))  # Above-Right
         if x != self.horizontal_tiles - 1:
-            tiles.append((x + 1, y)) # Right
+            tiles.append((x + 1, y, self.get_space(x, y)))  # Right
         if x != self.horizontal_tiles - 1 and y != self.vertical_tiles - 1:
-            tiles.append((x + 1, y + 1)) # Bottom-Right
+            tiles.append((x + 1, y + 1, self.get_space(x, y)))  # Bottom-Right
         if y != self.vertical_tiles - 1:
-            tiles.append((x, y + 1)) # Bottom
+            tiles.append((x, y + 1, self.get_space(x, y)))  # Bottom
         if x != 0 and y != self.vertical_tiles - 1:
-            tiles.append((x - 1, y + 1)) # Bottom-Left
+            tiles.append((x - 1, y + 1, self.get_space(x, y)))  # Bottom-Left
         if x != 0:
-            tiles.append((x - 1, y)) # Left
+            tiles.append((x - 1, y, self.get_space(x, y)))  # Left
         return tiles
-    
+
     def get_unsolved_tiles(self):
         tiles = []
+        x = -1
+        y = -1
         for row in self.board:
+            y += 1
+            x = -1
             for tile in row:
+                x += 1
                 if not tile.solved:
                     tiles.append((x, y, tile))
         return tiles
-    
+
     def solve_tiles(self):
         unsolved_tiles = self.get_unsolved_tiles()
         for tile in unsolved_tiles:
             solved = True
-            adj_tiles = self.get_surrounding_tiles()
+            adj_tiles = self.get_surrounding_tiles(tile[0], tile[1])
             for adj_tile in adj_tiles:
-                if adj_tile.value is None:
+                if adj_tile[2].value is None:
                     solved = False
                     break
             tile.solved = solved
-            
+
     def get_border_tiles(self):
         # TODO - Bell is about to ring
         pass
