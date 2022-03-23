fffbfc2d5240df1b088cb8379b9b875be2e33e99
Zberritt <zberritt@gmail.com>
2022-03-23 07:23:50 -0400

    Speeding up slightly

--- google_minesweeper_solver/main.py
+++ google_minesweeper_solver/main.py
@@ -18,13 +18,19 @@ def do_move(board):
     virtual_board.solve_tiles()
     move = ai.get_next_move(board.virtual_board)
     pyautogui.moveTo(board.get_mouse_position(move.x, move.y))
+    action_name = ""
+    if move.action == 0:
+        action_name = "Flag"
+    elif move.action == 1:
+        action_name = "Click"
+    print("({}, {}) - {}".format(move.x, move.y, move.action))
     if move.action == 0:
         pyautogui.click(button="right")
+        virtual_board.get_space(move.x, move.y).value = -1  # Change the value to a mine, no need to rescan
     elif move.action == 1:
         pyautogui.click(button="left")
-    print("({}, {}) - {}".format(move.x, move.y, move.action))
-    pyautogui.moveTo(1, 1)
-    virtual_board.populate_board(board.get_tile_values())
+        pyautogui.moveTo(1, 1)
+        virtual_board.populate_board(board.get_tile_values())
     time.sleep(.5)
     do_move(board)
 
