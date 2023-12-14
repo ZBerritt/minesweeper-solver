4937b7df1c1252a173ab6bc69572ea6400eaeee8
Zach Berritt <zberritt@gmail.com>
2023-12-14 08:03:33 -0500

    Improve logging

--- minesweeper_solver/main.py
+++ minesweeper_solver/main.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import time
 import pyautogui
 from games.game import Game
@@ -7,8 +8,9 @@ from games.game_factory import game_factory
 
 def solver():
     args = parse_args()
+    logging.basicConfig(level=args.loglevel, format="%(message)s")
 
-    print("Scanning for Minesweeper board...")
+    logging.info("Scanning for Minesweeper board...")
     game = game_factory(args.type)
 
     if game is None:
@@ -18,11 +20,12 @@ def solver():
     print(f"{game.name} board detected! Beginning solver.")
     print("Note: To escape, move your cursor to the top-left corner of your screen")
 
-    game_status = do_move(game, flags=args.flags, verbose=args.verbose, delay=args.delay)
+    game_status = do_move(game, flags=args.flags, delay=args.delay)
 
     while game_status == 0:
-        game_status = do_move(game, flags=args.flags, verbose=args.verbose, delay=args.delay)
+        game_status = do_move(game, flags=args.flags, delay=args.delay)
 
+    game.board.print()
     if game_status == 1:
         print("Game Over...")
     elif game_status == 2:
@@ -30,19 +33,18 @@ def solver():
     elif game_status == 3:
         print("No more moves can be found...")
 
-def do_move(game: Game, flags=False, verbose=False, delay=None) -> int:
+def do_move(game: Game, flags=False, delay=None) -> int:
     pyautogui.moveTo(1, 1)
     time.sleep(delay if delay is not None else game.delay)
     game.update()
+    logging.info("Getting next move...")
     moves = get_next_moves(game.board)
     if not moves:
         return 3
     for move in moves:
         board_x, board_y, action = move
         screen_x, screen_y = game.get_mouse_position(board_x, board_y)
-
-        if verbose:
-            print(f"({board_x}, {board_y}) - {'Mine' if action == Action.FLAG else 'Safe'}")
+        logging.info(f"({board_x}, {board_y}) - {'Mine' if action == Action.FLAG else 'Safe'}")
             
         if action == Action.CLICK:
             pyautogui.click(x=screen_x, y=screen_y, button="left")
@@ -56,7 +58,7 @@ def parse_args() -> argparse.Namespace:
         description="A bot for solving the game minesweeper"
     )
     parser.add_argument("type", help="The Minesweeper game type", choices=["google"])
-    parser.add_argument("-v", "--verbose", help="Show the logs of the solver", action="store_true")
+    parser.add_argument("-v", "--verbose", help="Show the logs of the solver", action="store_const", dest="loglevel", const=logging.INFO)
     parser.add_argument("-f", "--flags", help="Flag mines", action="store_true")
     parser.add_argument("-d", "--delay", help="Override delay in seconds before each move (WARNING: Can break on certain games)", type=int)
 
